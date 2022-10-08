1a78c30b78c2a91242bc8ed656c069ee09f431af
Peachalex <conquereralex@gmail.com>
2022-10-08 10:20:29 -0400

    update

---
+++ average_numbers/target/debug/average_numbers.d
@@ -0,0 +1,1 @@
+/home/siyuan/Rust-practice/average_numbers/target/debug/average_numbers: /home/siyuan/Rust-practice/average_numbers/src/main.rs
---
+++ average_numbers/target/debug/deps/average_numbers-908911bf0c32a740.d
@@ -0,0 +1,5 @@
+/home/siyuan/Rust-practice/average_numbers/target/debug/deps/average_numbers-908911bf0c32a740: src/main.rs
+
+/home/siyuan/Rust-practice/average_numbers/target/debug/deps/average_numbers-908911bf0c32a740.d: src/main.rs
+
+src/main.rs:
