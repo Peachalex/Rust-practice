33878b7750eb1383efb4511daee91f21ab79d1ed
peachalex <conquereralex@gmail.com>
2022-10-14 17:21:59 -0400

    update

--- calculation_of_array/src/main.rs
+++ calculation_of_array/src/main.rs
@@ -1,18 +1,20 @@
 fn main() {
     let numbers = [1,9,-2,0,23,20,-7,13,37,20,56,-18,-20,3];
-    let mut max: i32 = numbers[0];
-    let mut min: i32 = numbers[0];
-    let mut sum :i32 = 0;
+    let mut max: i32 ;
+    let mut min: i32 ;
+    let mut mean :f64;
+    max = numbers[0];
+    min = numbers[0];
+    mean = 0.0;
     for i in 0..numbers.len(){
-         sum += numbers[i];
          if numbers[i] > max{
             max = numbers[i];
          }else if numbers[i] < min {
             min = numbers[i];
          }
+         mean += numbers[i] as f64;
     }
-    let mean : i64 = (sum as usize / numbers.len()).try_into().unwrap();
-    println!("sum: {sum}");
+    mean /= numbers.len() as f64;
     println!("mean: {mean}");
     println!("max: {max}");
     println!("min: {min}");
